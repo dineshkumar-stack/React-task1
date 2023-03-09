cd1eb9229bfae2591aa0da4e3e8c43f0a3018cdf
dineshkumar-stack <stardinesh4@gmail.com>
2023-03-09 20:41:46 +0530

    class notes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Detail
+        access="✔ "
+        img="https://thumbs.dreamstime.com/b/cute-boy-face-cartoon-vector-illustration-graphic-design-cute-boy-face-cartoon-110654260.jpg"
+        name="Dinesh"
+        batch="B34"
+      />
+      <Detail
+        access="❌ "
+        img="https://thumbs.dreamstime.com/b/cute-boy-face-cartoon-cute-boy-face-cartoon-vector-illustration-graphic-design-110654225.jpg"
+        name="Kumar"
+        batch="B35"
+      />
+      <Detail
+        access="❌ "
+        img="https://thumbs.dreamstime.com/b/cute-boy-face-cartoon-cute-boy-face-cartoon-vector-illustration-graphic-design-110654225.jpg"
+        name="Vignesh"
+        batch="B64"
+      />
+      <Detail
+        access="✔ "
+        img="https://thumbs.dreamstime.com/b/beautiful-girl-face-cartoon-beautiful-girl-face-cartoon-vector-illustration-graphic-design-110656458.jpg"
+        name="Ram"
+        batch="B24"
+      />
     </div>
   );
 }
 
-export default App;
+function Detail(props) {
+  return (
+    <div className="card">
+      <img src={props.img} alt={props.name} />
+      <p>Team member</p>
+      <h2>
+        <span>{props.access}</span>
+        {props.name}
+      </h2>
+      <h4>{props.batch}</h4>
+    </div>
+  );
+}
+
