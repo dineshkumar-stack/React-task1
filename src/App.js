58420c9f6eee95b78331effabc2997186020821a
dineshkumar-stack <stardinesh4@gmail.com>
2023-04-23 19:32:34 +0530

    Final it

--- src/App.js
+++ src/App.js
@@ -1,48 +1,125 @@
-import logo from './logo.svg';
 import './App.css';
+import { Card } from 'react-bootstrap';
+import { ListGroup } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
+
 
 export default function App() {
   return (
     <div className="App">
-      <Detail
-        access="✔ "
-        img="https://thumbs.dreamstime.com/b/cute-boy-face-cartoon-vector-illustration-graphic-design-cute-boy-face-cartoon-110654260.jpg"
-        name="Dinesh"
-        batch="B34"
+      <Detail1
+        type="FREE"
+        price="$0"
+        user="✔ Single User"
+        storage="✔ 5GB Storage"
+        project="✔ Unlimited Public Projects"
+        community="✔ Community Access"
+        limit="❌ Unlimited Private Projects"
+        support="❌ Dedicated Phone Support"
+        domain="❌ Free Subdomain"
+        status="❌ Monthly Status Reports"
+        yes="✔ "
+        no="❌ "
       />
-      <Detail
-        access="❌ "
-        img="https://thumbs.dreamstime.com/b/cute-boy-face-cartoon-cute-boy-face-cartoon-vector-illustration-graphic-design-110654225.jpg"
-        name="Kumar"
-        batch="B35"
+      <Detail2
+        type="PLUS"
+        price="$9"
+        user="✔ 5 Users"
+        storage="✔ 50GB Storage"
+        project="✔ Unlimited Public Projects"
+        community="✔ Community Access"
+        limit="✔ Unlimited Private Projects"
+        support="✔ Dedicated Phone Support"
+        domain="✔ Free Subdomain"
+        status="❌ Monthly Status Reports"
       />
-      <Detail
-        access="❌ "
-        img="https://thumbs.dreamstime.com/b/cute-boy-face-cartoon-cute-boy-face-cartoon-vector-illustration-graphic-design-110654225.jpg"
-        name="Vignesh"
-        batch="B64"
-      />
-      <Detail
-        access="✔ "
-        img="https://thumbs.dreamstime.com/b/beautiful-girl-face-cartoon-beautiful-girl-face-cartoon-vector-illustration-graphic-design-110656458.jpg"
-        name="Ram"
-        batch="B24"
+      <Detail3
+        type="PRO"
+        price="$49"
+        user="✔ Unlimited Users"
+        storage="✔ 150GB Storage"
+        project="✔ Unlimited Public Projects"
+        community="✔ Community Access"
+        limit="✔ Unlimited Private Projects"
+        support="✔ Dedicated Phone Support"
+        domain="✔ Unlimited Free Subdomains"
+        status="✔ Monthly Status Reports"
       />
     </div>
   );
+
 }
 
-function Detail(props) {
+function Detail1(props) {
   return (
-    <div className="card">
-      <img src={props.img} alt={props.name} />
-      <p>Team member</p>
-      <h2>
-        <span>{props.access}</span>
-        {props.name}
-      </h2>
-      <h4>{props.batch}</h4>
-    </div>
+    <Card class="card">
+
+      <Card.Header> <span class="he">{props.type}</span><br></br>{props.price}<span>/month</span></Card.Header>
+      <ListGroup variant="flush">
+        <ListGroup>{props.user}</ListGroup>
+        <ListGroup>{props.storage}</ListGroup>
+        <ListGroup>{props.project}</ListGroup>
+        <ListGroup>{props.community}</ListGroup>
+        <ListGroup variant="not">{props.limit}</ListGroup>
+        <ListGroup variant="not">{props.support}</ListGroup>
+        <ListGroup variant="not">{props.domain}</ListGroup>
+        <ListGroup variant="not">{props.status}</ListGroup>
+        <Button variant="light">Buy</Button>{' '}
+
+      </ListGroup>
+
+    </Card>
+
   );
 }
 
+
+function Detail2(props) {
+  return (
+    <Card class="card">
+
+      <Card.Header> <span class="he">{props.type}</span><br></br>{props.price}<span>/month</span></Card.Header>
+      <ListGroup variant="flush">
+        <ListGroup>{props.user}</ListGroup>
+        <ListGroup>{props.storage}</ListGroup>
+        <ListGroup>{props.project}</ListGroup>
+        <ListGroup>{props.community}</ListGroup>
+        <ListGroup>{props.limit}</ListGroup>
+        <ListGroup>{props.support}</ListGroup>
+        <ListGroup>{props.domain}</ListGroup>
+        <ListGroup variant="not">{props.status}</ListGroup>
+        <Button variant="light">Buy</Button>{' '}
+
+      </ListGroup>
+
+    </Card>
+
+  );
+}
+
+
+
+function Detail3(props) {
+  return (
+    <Card class="card">
+
+      <Card.Header> <span class="he">{props.type}</span><br></br>{props.price}<span>/month</span></Card.Header>
+      <ListGroup variant="flush">
+        <ListGroup>{props.user}</ListGroup>
+        <ListGroup>{props.storage}</ListGroup>
+        <ListGroup>{props.project}</ListGroup>
+        <ListGroup>{props.community}</ListGroup>
+        <ListGroup>{props.limit}</ListGroup>
+        <ListGroup>{props.support}</ListGroup>
+        <ListGroup>{props.domain}</ListGroup>
+        <ListGroup>{props.status}</ListGroup>
+        <Button variant="light">Buy</Button>{' '}
+
+      </ListGroup>
+
+    </Card>
+
+  );
+}
